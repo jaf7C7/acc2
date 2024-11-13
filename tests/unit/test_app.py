ad9efe112ea7073c43bf55b485b10a21a87df4b1
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-13 14:18:17 +0000

    Refactor unit tests

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,24 +1,24 @@
+import pytest
 from unittest.mock import Mock
 from acc.app import Application
 
 
-def test_empty_date_param_returns_current_date():
-    config = Mock()
-    config.date = '1970-01-01'
-    app = Application(config)
+@pytest.fixture
+def app():
+    return Application(Mock())
+
+
+def test_empty_date_param_returns_current_date(app):
+    app.config.date = '1970-01-01'
     assert app.run({'date': ''}) == {'date': '1970-01-01'}
 
 
-def test_non_empty_date_param_sets_new_date():
-    config = Mock()
-    config.date = '1970-01-01'
-    app = Application(config)
+def test_non_empty_date_param_sets_new_date(app):
+    app.config.date = '1970-01-01'
     app.run({'date': '1999-12-31'})
     assert app.config.date == '1999-12-31'
 
 
-def test_empty_ledger_param_returns_current_ledger():
-    config = Mock()
-    config.ledger = 'my_ledger'
-    app = Application(config)
+def test_empty_ledger_param_returns_current_ledger(app):
+    app.config.ledger = 'my_ledger'
     assert app.run({'ledger': ''}) == {'ledger': 'my_ledger'}
