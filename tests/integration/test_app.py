571d77c4a8ec20039d0c4119c7495a4e89800d9c
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-13 16:52:20 +0000

    Fix error in test

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -18,4 +18,4 @@ def test_set_and_get_new_date():
 
 def test_empty_ledger_param_returns_current_ledger():
     app = Application()
-    assert app.run({'ledger': ''}) == 'ledger'
+    assert app.run({'ledger': ''}) == {'ledger': 'ledger'}
