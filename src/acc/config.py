2dfc08f26c944a845c303365cd9e4353ac13711c
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-13 13:07:24 +0000

    Setting `Config.date` writes new value to file

--- src/acc/config.py
+++ src/acc/config.py
@@ -3,4 +3,14 @@ class Config:
 
     def __init__(self, path='.acc.conf'):
         self.path = path
-        self.date = '1970-01-01'
+        self._date = '1970-01-01'
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, new_date):
+        self._date = new_date
+        with open(self.path, mode='w') as f:
+            f.write(new_date)
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -6,3 +6,12 @@ def test_returns_default_date_if_config_file_doesnt_exist():
     config = Config()
     assert not os.path.exists(config.path)
     assert config.date == '1970-01-01'
+
+def test_setting_date_property_writes_new_value_to_file():
+    config = Config()
+    assert not os.path.exists(config.path)
+    config.date = '2020-01-01'
+    with open(config.path) as f:
+        date = f.read()
+    assert date == '2020-01-01'
+    os.unlink(config.path)
