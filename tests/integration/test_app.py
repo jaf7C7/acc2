6347e37246849cb89d5238f16014796f017a6663
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-13 14:05:47 +0000

    Add integration test for getting/setting application date

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,8 +1,16 @@
 """Test the integration of the Application and Config objects."""
 
+import os
 from acc.app import Application
 
 
 def test_empty_date_param_returns_default_date():
     app = Application()
     assert app.run({'date': ''}) == {'date': '1970-01-01'}
+
+
+def test_set_and_get_new_date():
+    app = Application()
+    app.run({'date': '2020-01-01'})
+    assert app.run({'date': ''}) == {'date': '2020-01-01'}
+    os.unlink(app.config.path)
