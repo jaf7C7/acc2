f4a7f2cd5c8ef84723f8466cd545cbc4b203bca1
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-13 16:51:42 +0000

    Rename tests

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -4,7 +4,7 @@ import os
 from acc.app import Application
 
 
-def test_empty_date_param_returns_default_date():
+def test_empty_date_param_returns_current_date():
     app = Application()
     assert app.run({'date': ''}) == {'date': '1970-01-01'}
 
@@ -16,6 +16,6 @@ def test_set_and_get_new_date():
     os.unlink(app.config.path)
 
 
-def test_empty_ledger_param_returns_default_ledger():
+def test_empty_ledger_param_returns_current_ledger():
     app = Application()
     assert app.run({'ledger': ''}) == 'ledger'
