d7455fcb738d47b9e4e1a3984f3a4f60291ef89b
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-13 14:08:02 +0000

    Rename test

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -2,7 +2,7 @@ from unittest.mock import Mock
 from acc.app import Application
 
 
-def test_empty_date_param_returns_default_date():
+def test_empty_date_param_returns_current_date():
     config = Mock()
     config.date = '1970-01-01'
     app = Application(config)
