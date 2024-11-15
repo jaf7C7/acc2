56cc535a4d988863bfa89a33ef17f491dc0dd888
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-15 14:51:38 +0000

    Remove `Config` class

--- src/acc/app.py
+++ src/acc/app.py
@@ -1,16 +1,18 @@
 from datetime import date
-from acc.config import Config
 from acc.models import CSVFile
 
 
 class Application:
     """The central object which handles the main application logic."""
 
-    def __init__(self, config=None, ledger_type=CSVFile):
-        if config is None:
-            config = Config()
-        self.config = config
-        self.ledger = ledger_type(self.config.ledger)
+    def __init__(
+        self, config_path='acc.conf', config_type=CSVFile, ledger_type=CSVFile
+    ):
+        self.config_path = config_path
+        self.config_type = config_type
+        self.ledger_path = 'ledger'
+        self.ledger_type = ledger_type
+        self.date = '1970-01-01'
 
     def run(self, args):
         """Runs the application with the given arguments."""
@@ -20,25 +22,26 @@ class Application:
                     date.fromisoformat(args['date'])
                 except ValueError as e:
                     raise (e)
-                self.config.date = args['date']
-            return {'date': self.config.date}
+                self.date = args['date']
+            return {'date': self.date}
 
         elif 'ledger' in args:
             if args['ledger'] != '':
-                self.config.ledger = args['ledger']
-            return {'ledger': self.config.ledger}
+                self.ledger_path = args['ledger']
+            return {'ledger': self.ledger_path}
 
         elif 'transaction' in args:
+            ledger = self.ledger_type(self.ledger_path)
             try:
-                id = len(self.ledger.read())
+                id = len(ledger.read())
             except FileNotFoundError:
                 id = 0
             transaction = {
                 'id': id,
-                'date': self.config.date,
+                'date': self.date,
                 **args['transaction'],
             }
-            self.ledger.write([transaction], mode='a')
+            ledger.write([transaction], mode='a')
 
         elif 'report' in args:
-            return {'transactions': self.ledger.read()}
+            return {'transactions': ledger.read()}
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -5,18 +5,18 @@ from acc.app import Application
 
 @pytest.fixture
 def app():
-    return Application(MagicMock(), ledger_type=MagicMock())
+    return Application(config_type=MagicMock(), ledger_type=MagicMock())
 
 
 def test_empty_date_param_returns_current_date(app):
-    app.config.date = '1970-01-01'
+    app.date = '1970-01-01'
     assert app.run({'date': ''}) == {'date': '1970-01-01'}
 
 
 def test_non_empty_date_param_sets_new_date(app):
-    app.config.date = '1970-01-01'
+    app.date = '1970-01-01'
     app.run({'date': '1999-12-31'})
-    assert app.config.date == '1999-12-31'
+    assert app.date == '1999-12-31'
 
 
 def test_exception_raised_if_new_date_is_not_a_valid_iso_format_date(app):
@@ -25,14 +25,14 @@ def test_exception_raised_if_new_date_is_not_a_valid_iso_format_date(app):
 
 
 def test_empty_ledger_param_returns_current_ledger(app):
-    app.config.ledger = 'my_ledger'
+    app.ledger_path = 'my_ledger'
     assert app.run({'ledger': ''}) == {'ledger': 'my_ledger'}
 
 
 def test_non_empty_ledger_param_sets_new_ledger(app):
-    app.config.ledger = 'old_ledger'
+    app.ledger_path = 'old_ledger'
     app.run({'ledger': 'new_ledger'})
-    assert app.config.ledger == 'new_ledger'
+    assert app.ledger_path == 'new_ledger'
 
 
 def test_can_record_transactions(app):
@@ -42,11 +42,12 @@ def test_can_record_transactions(app):
         'description': 'Toilet paper (multipack).',
     }
     app.run({'transaction': transaction})
-    app.ledger.write.assert_called_with(
+    ledger = app.ledger_type(app.ledger_path)
+    ledger.write.assert_called_with(
         [
             {
                 'id': 0,
-                'date': app.config.date,
+                'date': app.date,
                 **transaction,
             }
         ],
