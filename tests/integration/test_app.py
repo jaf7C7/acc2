e2bc7bc33f7df1f9797c8469abb1b287303812ca
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-15 19:10:08 +0000

    Fix broken integration tests

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -5,55 +5,39 @@ from acc.app import Application
 
 @pytest.fixture
 def app(tmp_path):
-    config_path = os.path.join(tmp_path, 'test_config')
-    ledger_path = os.path.join(tmp_path, 'test_ledger')
+    os.chdir(tmp_path)
     app = Application()
-    app.config_path = config_path
-    app.ledger_path = ledger_path
     yield app
-    for f in config_path, ledger_path:
+    for f in app.config_path, app.ledger_path:
         try:
             os.unlink(f)
         except FileNotFoundError:
             pass
 
 
-def test_empty_date_param_returns_current_date(app):
-    assert app.run({'date': ''}) == {'date': app.date}
-
-
 def test_set_and_get_new_date(app):
-    app.run({'date': '2020-01-01'})
-    assert app.run({'date': ''}) == {'date': '2020-01-01'}
-
-
-def test_empty_ledger_param_returns_current_ledger(app):
-    assert app.run({'ledger': ''}) == {'ledger': app.ledger_path}
+    app.set_date('2020-01-01')
+    assert app.get_date() == {'date': '2020-01-01'}
 
 
 def test_set_and_get_new_ledger(app):
-    app.run({'ledger': 'ledger'})
-    assert app.run({'ledger': ''}) == {'ledger': 'ledger'}
+    app.set_ledger('ledger')
+    assert app.get_ledger() == {'ledger': 'ledger'}
 
 
-def test_add_and_report_transactions(app):
-    app.run(
-        {
-            'transaction': {
-                'type': 'credit',
-                'amount': 1099,
-                'description': 'Gift from Grandma.',
-            }
-        }
-    )
-    assert app.run({'transactions': ''}) == {
+def test_add_and_get_transaction(app):
+    transaction = {
+        'type': 'credit',
+        'amount': '1099',
+        'description': 'Gift from Grandma.',
+    }
+    app.add_transaction(transaction)
+    assert app.get_transactions() == {
         'transactions': [
             {
                 'id': '0',
                 'date': str(app.date),
-                'type': 'credit',
-                'amount': '1099',
-                'description': 'Gift from Grandma.',
+                **transaction,
             }
         ]
     }
