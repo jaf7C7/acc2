ae3e3bae78acd7a9cfd7590e37a2c1df0cf0f49d
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-15 12:32:31 +0000

    Allow references to application object in tests.

--- src/acc/api.py
+++ src/acc/api.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, g
 from acc.app import Application
 
 
 api = Flask(__name__)
 
 
+def get_app():
+    if 'app' not in g:
+        g.app = Application()
+    return g.app
+
+
 @api.route('/date')
 def get_date():
-    app = Application()
+    app = get_app()
     return app.run({'date': ''})
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,5 +1,6 @@
 import pytest
-from acc.api import api
+from flask import g
+from acc.api import api, get_app
 from acc.app import Application
 
 
@@ -8,6 +9,18 @@ def client():
     return api.test_client()
 
 
-def test_get_date_endpoint(client):
-    app = Application()
+@pytest.fixture
+def app():
+    with api.app_context():
+        g.app = Application()
+        yield g.app
+
+
+def test_get_app():
+    with api.app_context():
+        app = get_app()
+        assert app is get_app()
+
+
+def test_get_date_endpoint(app, client):
     assert client.get('/date').get_json() == app.run({'date': ''})
