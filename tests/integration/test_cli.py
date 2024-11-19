ae3be72a3ab0323e37a1050e22858593552c7346
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-19 14:22:20 +0000

    Remove unused tests

--- tests/integration/test_cli.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import Mock
-import pytest
-import acc.cli as cli
-
-
-def test_get_date(capsys):
-    cli.run(['date'])
-    out, err = capsys.readouterr()
-    assert out == '1970-01-01\n'
