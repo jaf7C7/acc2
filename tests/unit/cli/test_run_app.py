a68fe7803c2205b3f528cc31d1a26ca055e94c28
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-19 11:41:40 +0000

    Make test names more descriptive

--- tests/unit/cli/test_run_app.py
+++ tests/unit/cli/test_run_app.py
@@ -15,7 +15,7 @@ def test_date_cmd_without_new_date_prints_current_date(app, capsys):
     assert out == f'{app.get_date()}\n'
 
 
-def test_run_app_calls_set_date(app):
+def test_date_cmd_with_new_date_sets_new_date(app):
     args = Mock(command='date', date='2000-01-01')
     run_app(args, app=app)
     app.set_date.assert_called_with('2000-01-01')
@@ -28,14 +28,14 @@ def test_ledger_cmd_without_new_ledger_prints_current_ledger_path(app, capsys):
     assert out == f'{app.ledger_path}\n'
 
 
-def test_run_app_sets_ledger_path(app):
+def test_ledger_cmd_with_new_ledger_sets_new_ledger_path(app):
     args = Mock(command='ledger', ledger='new_ledger')
     run_app(args, app=app)
     assert app.ledger_path == 'new_ledger'
 
 
 @pytest.mark.parametrize('command', ('debit', 'credit'))
-def test_run_app_calls_add_transaction(command, app):
+def test_debit_and_credit_cmds_with_valid_params_add_new_transaction(command, app):
     args = Mock(command=command, amount='1099', description='Marmite')
     run_app(args, app=app)
     app.add_transaction.assert_called_with(
