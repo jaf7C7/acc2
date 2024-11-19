c665474fb03cef23177c9d05814acbc5cee6aee7
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-19 11:23:11 +0000

    Refactoring

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -22,38 +22,38 @@ def app():
         yield g.app
 
 
-def test_get_app():
+def test_get_app_returns_same_object_each_time():
     with api.app_context():
         app = get_app()
         assert app is get_app()
 
 
-def test_get_date_endpoint(app, client):
+def test_get_date_endpoint_returns_the_date(app, client):
     date = client.get('/date').get_json()['date']
     assert date == app.get_date()
 
 
-def test_put_date_endpoint(app, client):
+def test_put_date_endpoint_sets_new_date(app, client):
     client.put('/date', json={'date': '2020-01-01'})
     app.set_date.assert_called_with('2020-01-01')
 
 
-def test_get_ledger_endpoint(app, client):
+def test_get_ledger_endpoint_returns_the_ledger_path(app, client):
     ledger = client.get('/ledger').get_json()['ledger']
     assert ledger == app.ledger_path
 
 
-def test_put_ledger_endpoint(app, client):
+def test_put_ledger_endpoint_sets_new_ledger_path(app, client):
     client.put('/ledger', json={'ledger': 'new_ledger'})
     assert app.ledger_path == 'new_ledger'
 
 
-def test_get_transaction_endpoint(app, client):
+def test_get_transaction_endpoint_returns_list_of_transactions(app, client):
     transactions = client.get('/transactions').get_json()['transactions']
     assert transactions == app.get_transactions()
 
 
-def test_post_transactions_endpoint(app, client):
+def test_post_transactions_endpoint_adds_new_transaction(app, client):
     transaction = {'foo': 1, 'bar': 2}
     client.post('/transactions', json=transaction)
     app.add_transaction.assert_called_with(transaction)
