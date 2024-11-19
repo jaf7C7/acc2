ac267f8a856e99ec69ac4878001a14939776bb16
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-11-19 12:36:46 +0000

    `run` accepts `ledger` command

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -20,3 +20,16 @@ def test_date_cmd_with_new_date_sets_new_date(app):
     new_date = '2000-01-01'
     run(['date', new_date], app=app)
     app.set_date.assert_called_with(new_date)
+
+
+def test_ledger_cmd_without_new_ledger_prints_current_ledger_path(app, capsys):
+    run(['ledger'], app=app)
+    out, err = capsys.readouterr()
+    current_ledger = app.ledger_path
+    assert out == f'{current_ledger}\n'
+
+
+def test_ledger_cmd_with_new_ledger_sets_new_ledger_path(app):
+    new_ledger = 'new_ledger'
+    run(['ledger', new_ledger], app=app)
+    assert app.ledger_path == new_ledger
